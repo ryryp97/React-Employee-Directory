79842b664e6e6e01a9572952e97c40cb822016f9
ryry <ryanpetrecca@gmail.com>
2021-03-13 17:22:23 -0800

    added code to display json data through a map method onto card components

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,61 +1,58 @@
-import React, { Component } from "react";
-import API from "../utils/API";
+import React, { useState } from "react";
 import Container from "../components/Container";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+import staff from "../staff.json";
+import EmployeeCard from "../components/EmployeeCard"
 
-class Gallery extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
-  };
+function Gallery() {
 
+  const [staffInfo, setStaffInfo] = useState({
+    staff: staff
+  })
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
+  const handleSelect = event => {
+    if (event.target.value === "All" || event.target.value === "View by Department") {
+      setStaffInfo({ ...staffInfo, staff: staff });
+    }
+    else {
+      const deptStaff = staff.filter(employee => employee.department === event.target.value);
+      setStaffInfo({ ...staffInfo, staff: deptStaff });
+    }
   }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
-  render() {
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Staff Directory</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+  return (
+    <div>
+        <h1 className="text-center">Staff Directory</h1>
+        <div className="row">
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            onChange={handleSelect}
           >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
-        </Container>
-      </div>
-    );
-  }
+            <option defaultValue>View by Department</option>
+            <option value="All">All</option>
+            <option value="Engineering">Engineering</option>
+            <option value="Design">Design</option>
+            <option value="Sales">Sales</option>
+            <option value="Legal">Legal</option>
+            <option value="Services">Services</option>
+          </select>
+        </div>
+
+        <div className="row">
+          {staffInfo.staff.map(employee => (
+            <EmployeeCard
+              id={employee.id}
+              key={employee.id}
+              name={employee.name}
+              image={employee.image}
+              role={employee.role}
+              department={employee.department}
+            />
+          ))}
+        </div>
+    </div>
+  );
+
 }
 
 export default Gallery;
